aefd6a4b4c8e04500424dcb3069f70c6a5f8acd7
darksoldier1404 <darksoldier1404@gmail.com>
2021-11-30 08:05:31 +0900

    1.12.2 전용 DUC

---
+++ v1_12_2/src/main/java/com.darksoldier1404.duc/utils/PluginUtil.java
@@ -0,0 +1,87 @@
+package com.darksoldier1404.duc.utils;
+
+import com.darksoldier1404.duc.UniversalCore;
+import com.darksoldier1404.duc.enums.PluginName;
+import com.darksoldier1404.dvs.VirtualStorage;
+import com.earth2me.essentials.Essentials;
+import me.clip.placeholderapi.PlaceholderAPIPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+@SuppressWarnings("all")
+public class PluginUtil {
+    private static final UniversalCore plugin = UniversalCore.getInstance();
+    private static final String prefix = plugin.prefix;
+    private static final PluginManager pm = Bukkit.getPluginManager();
+    private static final Logger log = plugin.getLogger();
+
+    public static void loadALLPlugins() {
+        new Metrics(plugin, 13426);
+        Plugin pl;
+        pl = pm.getPlugin("Essentials");
+        if (pl != null) {
+            plugin.ess = (Essentials) pl;
+        }
+        pl = pm.getPlugin("PlaceholderAPI");
+        if (pl != null) {
+            plugin.papi = (PlaceholderAPIPlugin) pl;
+        }
+        pl = pm.getPlugin("DP-LegendaryCash");
+        if (pl != null) {
+            plugin.getEnabledPlugins().put(PluginName.LegendaryCash, (JavaPlugin) pl);
+            log.info(prefix + " DP-LegendaryCash 플러그인 활성화.");
+            new Metrics((JavaPlugin) pl, 13387);
+        }
+        pl = pm.getPlugin("DP-VirtualStorage");
+        if (pl != null) {
+            VirtualStorage vs = (VirtualStorage) pl;
+            if(plugin.ess == null) {
+                log.warning(prefix + " 에센셜 플러그인이 감지되지 않음.");
+                log.warning(prefix + " Essentials 플러그인이 설치되어 있지 않아 DP-VirtualStorage 플러그인의 창고 구매 기능을 비활성화 합니다.");
+                vs.ess = null;
+            }else{
+                vs.ess = plugin.ess;
+                plugin.getEnabledPlugins().put(PluginName.VirtualStorage, (JavaPlugin) pl);
+                log.info(prefix + " DP-VirtualStorage 플러그인 활성화.");
+                new Metrics((JavaPlugin) pl, 13386);
+            }
+        }
+    }
+
+    public static boolean isPluginEnabled(JavaPlugin pl) {
+        return PluginUtil.plugin.getEnabledPlugins().containsKey(PluginName.valueOf(pl.getName()));
+    }
+
+    public static void enablePlugin(JavaPlugin pl) {
+        PluginUtil.plugin.getEnabledPlugins().put(PluginName.valueOf(pl.getName()), pl);
+        Bukkit.getPluginManager().enablePlugin(pl);
+    }
+
+    public static void disablePlugin(JavaPlugin pl) {
+        PluginUtil.plugin.getEnabledPlugins().remove(PluginName.valueOf(pl.getName()));
+        Bukkit.getPluginManager().disablePlugin(pl);
+    }
+
+    public static void reloadPlugin(JavaPlugin pl) {
+        PluginUtil.plugin.getEnabledPlugins().remove(PluginName.valueOf(pl.getName()));
+        Bukkit.getPluginManager().disablePlugin(pl);
+        PluginUtil.plugin.getEnabledPlugins().put(PluginName.valueOf(pl.getName()), pl);
+        Bukkit.getPluginManager().enablePlugin(pl);
+    }
+
+    public static boolean isUpdateCheckEnabled(PluginName name) {
+        return plugin.config.getBoolean("Settings."+name.getName()+".update-check");
+    }
+
+    public static long getUpdateCheckInterval(PluginName name) {
+        return plugin.config.getLong("Settings."+name.getName()+".update-check-interval");
+    }
+
+    public static boolean isMetricsEnabled(PluginName name) {
+        return plugin.config.getBoolean("Settings."+name.getName()+".use-metrics");
+    }
+}
---
+++ v1_12_2/src/main/java/com.darksoldier1404.duc/utils/Triple.java
@@ -0,0 +1,20 @@
+package com.darksoldier1404.duc.utils;
+
+public record Triple<A, B, C>(A a, B b, C c) {
+
+    public static <A, B, C> Triple<A, B, C> of(A a, B b, C c) {
+        return new Triple<>(a, b, c);
+    }
+
+    public A getA() {
+        return a;
+    }
+
+    public B getB() {
+        return b;
+    }
+
+    public C getC() {
+        return c;
+    }
+}
